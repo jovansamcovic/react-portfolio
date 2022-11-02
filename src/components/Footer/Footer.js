9e9e2f9cb42d947bbb7143144cbc9c8d1133c522
Jovan Samcovic <jsamcovic1996@gmail.com>
2022-11-02 14:47:50 +0100

    [React portfolio]
    - Added "Contact me" section
    - Added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <strong className='footer__title'>Samčović</strong>
+      <span>© 2022 Personal Portfolio Jovan Samčović.</span>
+      <span>All rights reserved.</span>
+    </div>
+  )
+};
+
+export default Footer;
